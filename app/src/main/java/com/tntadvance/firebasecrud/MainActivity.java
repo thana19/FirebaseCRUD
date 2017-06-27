ad11af423c5ed53b5c28f7ecddb5931ef9b89a8a
thana <thana.j@gmail.com>
2017-06-27 21:21:57 +0700

    update+delete dialog

--- app/src/main/java/com/tntadvance/firebasecrud/MainActivity.java
+++ app/src/main/java/com/tntadvance/firebasecrud/MainActivity.java
@@ -1,9 +1,12 @@
 package com.tntadvance.firebasecrud;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -50,6 +53,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnAdd = (Button) findViewById(R.id.btnAdd);
         btnAdd.setOnClickListener(this);
         listView = (ListView) findViewById(R.id.listView);
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                UserDao dao = users.get(i);
+                showDialog(dao);
+                return false;
+            }
+        });
 
         users = new ArrayList<>();
 
@@ -102,6 +113,80 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
+            }
+        });
+
+    }
+
+    private boolean updateUser(String id, String name, String group) {
+        //updating
+        UserDao dao = new UserDao();
+        dao.setId(id);
+        dao.setName(name);
+        dao.setGroup(group);
+
+        firebaseReference.child("users").child(id).setValue(dao);
+        Toast.makeText(this, "User Updated", Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+    private boolean deleteUser(String id) {
+        //removing
+        firebaseReference.child("users").child(id).removeValue();
+
+        Toast.makeText(this, "User Deleted", Toast.LENGTH_LONG).show();
+
+        return true;
+    }
+
+
+    private int getIndex(Spinner spinner, String myString) {
+        int index = 0;
+
+        for (int i = 0; i < spinner.getCount(); i++) {
+            if (spinner.getItemAtPosition(i).toString().equalsIgnoreCase(myString)) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    private void showDialog(final UserDao dao) {
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.user_dialog, null);
+        dialogBuilder.setView(dialogView);
+
+        final EditText edName = (EditText) dialogView.findViewById(R.id.edName);
+        final Spinner spinnerGroup = (Spinner) dialogView.findViewById(R.id.spinnerGroup);
+        final Button btnUpdate = (Button) dialogView.findViewById(R.id.btnUpdate);
+        final Button btnDelete = (Button) dialogView.findViewById(R.id.btnDelete);
+
+        dialogBuilder.setTitle(dao.getName());
+        edName.setText(dao.getName());
+        spinnerGroup.setSelection(getIndex(spinnerGroup, dao.getGroup()));
+        final AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = edName.getText().toString().trim();
+                String genre = spinnerGroup.getSelectedItem().toString();
+                if (!TextUtils.isEmpty(name)) {
+                    updateUser(dao.getId(), name, genre);
+                    alertDialog.dismiss();
+                }
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteUser(dao.getId());
+                alertDialog.dismiss();
             }
         });
 
